e6114f0bb2372a6476392017b80346f92af57e4e
sktran01 <sktrn88@yahoo.com>
2022-04-20 16:52:05 -0500

    votes

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,7 @@
+DROP TABLE IF EXISTS votes;
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS voters;
 
 CREATE TABLE parties (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -24,3 +26,15 @@ CREATE TABLE voters (
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE votes (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  voter_id INTEGER NOT NULL,
+  candidate_id INTEGER NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  -- make sure voter only votes once i.e voter id is unique
+  CONSTRAINT uc_voter UNIQUE (voter_id),
+  -- on delete cascade-the entire row will be delete i.e vote remove if voter or candidate remove from database
+  CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
+  CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
+);
+  
